4fec47291840c5f3252f38cffb5537d2adbb66d0
abulyousuf <abulyousuf@btinternet.com>
2021-12-02 17:53:14 +0000

    Update script

--- boredbot/index.js
+++ boredbot/index.js
@@ -1,10 +1,13 @@
 const btn = document.querySelector("#btn");
 const activity = document.querySelector("#activity");
+const title = document.querySelector("#title");
 
 btn.addEventListener("click", () => {
     fetch("https://www.boredapi.com/api/activity")
         .then(response => response.json())
         .then(data => {
             activity.textContent = data.activity;
+            title.textContent = "🦾 HappyBot 🦿";
+            document.body.classList.add("fun");
         })
 });
