8008cb08cda6605ba4acd427f1de89272b985e78
abulyousuf <abulyousuf@btinternet.com>
2021-12-07 16:57:40 +0000

    Draw cards from deck

--- game-of-war/index.js
+++ game-of-war/index.js
@@ -10,4 +10,14 @@ const handleClick = () => {
         });
 };
 
+const drawCards = () => {
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        });
+};
+
 document.querySelector("#new-deck").addEventListener("click", handleClick);
+
+document.querySelector("#draw-cards").addEventListener("click", drawCards);
