25ede5bf7bd5b4c3b73cd21a1dcea3596be130d2
abulyousuf <abulyousuf@btinternet.com>
2021-12-07 20:43:58 +0000

    Refactor card image placement

--- game-of-war/index.js
+++ game-of-war/index.js
@@ -1,12 +1,14 @@
 let deckId = "";
 
+const cardsContainer = document.querySelector("#cards");
+const newDeckBtn = document.querySelector("#new-deck");
+const drawCardsBtn = document.querySelector("#draw-cards");
+
 const handleClick = () => {
     fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
             deckId = data.deck_id;
-
-            console.log(deckId);
         });
 };
 
@@ -14,15 +16,14 @@ const drawCards = () => {
     fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            console.log(data);
-
-            document.querySelector("#cards").innerHTML = `
-                <img src="${data.cards[0].image}" alt="card">
-                <img src="${data.cards[1].image}" alt="card">
-            `;
+            for (let i = 0; i < cardsContainer.children.length; i++) {
+                cardsContainer.children[i].innerHTML = `
+                    <img src="${data.cards[i].image}" alt="card" class="card">
+                `;
+            }
         });
 };
 
-document.querySelector("#new-deck").addEventListener("click", handleClick);
+newDeckBtn.addEventListener("click", handleClick);
 
-document.querySelector("#draw-cards").addEventListener("click", drawCards);
+drawCardsBtn.addEventListener("click", drawCards);
