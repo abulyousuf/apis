c029e7e8fa535ba9ba26a70cf6e966dfb4900fb9
abulyousuf <abulyousuf@btinternet.com>
2021-12-07 17:25:27 +0000

    Display card images

--- game-of-war/index.js
+++ game-of-war/index.js
@@ -15,6 +15,11 @@ const drawCards = () => {
         .then(res => res.json())
         .then(data => {
             console.log(data);
+
+            document.querySelector("#cards").innerHTML = `
+                <img src="${data.cards[0].image}" alt="card">
+                <img src="${data.cards[1].image}" alt="card">
+            `;
         });
 };
 
