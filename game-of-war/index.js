9686ada6df6e6473610db13907f785f89e78c156
abulyousuf <abulyousuf@btinternet.com>
2021-12-10 17:08:22 +0000

    Display remaining cards when drawing

--- game-of-war/index.js
+++ game-of-war/index.js
@@ -4,6 +4,7 @@ const cardsContainer = document.querySelector("#cards");
 const newDeckBtn = document.querySelector("#new-deck");
 const drawCardsBtn = document.querySelector("#draw-cards");
 const header = document.querySelector("#header");
+const remainingCards = document.querySelector("#remaining");
 
 const handleClick = () => {
     fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
@@ -32,6 +33,8 @@ const drawCards = () => {
     fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
+            remainingCards.textContent = `Remaining cards: ${data.remaining}`;
+
             for (let i = 0; i < cardsContainer.children.length; i++) {
                 cardsContainer.children[i].innerHTML = `
                     <img src="${data.cards[i].image}" alt="card" class="card">
